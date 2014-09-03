af0f597cdcd1b90e91bb7b704cb89338e8551d6b
darryl west <darryl.west@RainCitySoftware.com>
2014-09-03 16:42:49 -0700

    added mset tests

--- test/MockRedisClientTests.js
+++ test/MockRedisClientTests.js
@@ -5,11 +5,26 @@
  */
 var should = require('chai').should(),
     dash = require('lodash' ),
+    casual = require('casual'),
+    uuid = require('node-uuid'),
     MockLogger = require('simple-node-logger' ).mocks.MockLogger,
     MockRedisClient = require('../lib/MockRedisClient');
 
 describe('MockRedisClient', function() {
     'use strict';
+
+    var createPairs = function(count) {
+        if (!count) count = 25;
+
+        var pairs = [];
+
+        while (count-- > 0) {
+            pairs.push( uuid.v4() );
+            pairs.push( casual.sentence );
+        }
+
+        return pairs;
+    };
 
     describe('#instance', function() {
         var mock = new MockRedisClient();
@@ -24,5 +39,25 @@ describe('MockRedisClient', function() {
 
             methods.length.should.equal( 122 );
         });
+    });
+
+    describe('#mset/mget', function() {
+        var mock = new MockRedisClient();
+
+        it('should set a list of key/values', function(done) {
+            var callback,
+                pairs = createPairs();
+
+            callback = function(err, status) {
+                should.not.exist( err );
+                should.exist( status );
+
+                done();
+            };
+
+            mock.mset( pairs, callback );
+        });
+
+        it('should get a set of values from keys');
     });
 });
