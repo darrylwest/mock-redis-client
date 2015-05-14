d86c64ffce5d665a6eb860ac0a1e58278fae18e0
Joseph Lokahi Podwys <jpodwys@ldschurch.org>
2015-05-14 15:51:03 -0600

    Updating unit test.

--- test/MockRedisClientTests.js
+++ test/MockRedisClientTests.js
@@ -37,7 +37,7 @@ describe('MockRedisClient', function() {
         it('should have all known methods by count', function() {
             var methods = dash.methods( mock );
 
-            methods.length.should.equal( 123 );
+            methods.length.should.equal( 127 );
         });
     });
 
